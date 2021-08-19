855449a0a9810cc6c33a5345037d3491f31b7366
Aksarium <xekytok@gmail.com>
2021-08-19 19:21:10 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstrProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's display current system's date & time");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
